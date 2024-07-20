b7bc8fd249edff7c4e2baa2d570e6192b756aec0
Denis Skvortsov <denisskvrtsv@gmail.com>
2024-07-20 22:14:42 +0200

    fix comment part 1

--- cypress/src/test.js
+++ cypress/src/test.js
@@ -1,5 +1,8 @@
+// Import an apiUiFixtures object that links API and UI objects for tests
 import { apiUiFixtures } from '../e2e/fixtures/apiUiFixtures';
 
+// Destructuring api and ui objects from apiUiFixtures
 const { ui,api } = apiUiFixtures;
 
+// Export api and ui objects for use in tests
 export { ui,api };
